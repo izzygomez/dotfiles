cc106680831b73330ba1494e8d1f45a248e87cdb
Izzy Gomez <izzygmit@gmail.com>
2024-03-13 02:48:39 -0400

    `tmux-powerline` disable mouse mode

--- tmux/tmux-powerline/config.sh
+++ tmux/tmux-powerline/config.sh
@@ -153,7 +153,7 @@ fi
 	export TMUX_POWERLINE_SEG_MODE_INDICATOR_PREFIX_MODE_TEXT="^"
 	export TMUX_POWERLINE_SEG_MODE_INDICATOR_PREFIX_MODE_TEXT_COLOR=""
 	# Whether the mouse mode section should be enabled. Should be {"true, "false"}.
-	export TMUX_POWERLINE_SEG_MODE_INDICATOR_MOUSE_MODE_ENABLED="true"
+	export TMUX_POWERLINE_SEG_MODE_INDICATOR_MOUSE_MODE_ENABLED="false"
 	# Mouse mode text & color overrides. Defaults to "mouse" & the segment foreground color set in the theme used.
 	export TMUX_POWERLINE_SEG_MODE_INDICATOR_MOUSE_MODE_TEXT="🐭"
 	export TMUX_POWERLINE_SEG_MODE_INDICATOR_MOUSE_MODE_TEXT_COLOR=""
