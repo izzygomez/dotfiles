d803fc692fe3475d85c46e189352507c02d6ee70
Izzy Gomez <izzy@izzygomez.com>
2025-12-03 01:48:53 -0500

    `firefox`: disable tab groups

--- firefox/user.js
+++ firefox/user.js
@@ -27,3 +27,6 @@ user_pref("full-screen-api.warning.timeout", 0);
 user_pref("full-screen-api.macos", false);
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
+
+// Turn off tab groups
+user_pref("browser.tabs.groups.enabled", false);
