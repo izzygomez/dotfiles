efdf254f4185cc95c77cdf75b9c39fbffd26b51b
Izzy Gomez <izzy@izzygomez.com>
2026-01-10 23:51:04 -0500

    customize command_execution settings to my liking

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -568,9 +568,9 @@ local baby_blue='152' # converted from hex '#ADD8E6', which is what I used in
 
   ###################[ command_execution_time: duration of the last command ]###################
   # Show duration of the last command if takes at least this many seconds.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=1
   # Show this many fractional digits. Zero means round to seconds.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=1
   # Execution time color.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=101
   # Duration format: 1d 2h 3m 4s.
